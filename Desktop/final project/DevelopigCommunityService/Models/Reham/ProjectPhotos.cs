43a776ef2979c79ee1c480787715a129a34125d1
Ahmed Elbassal <ahmed.bassal.italy@gmail.com>
2021-06-10 01:29:34 +0200

    initial

---
+++ Desktop/final project/DevelopigCommunityService/Models/Reham/ProjectPhotos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevelopigCommunityService.Models.Reham
+{
+    public class ProjectPhotos
+    {
+    }
+}
